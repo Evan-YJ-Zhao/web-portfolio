eee1a31963001815ef226a17648943b98259b66d
Yu-Jun-Zhao <yujun.zhao4@gmail.com>
2024-11-06 14:12:26 -0800

    aligned items in lists

--- components/TechStackPanel/TechStackPanel.tsx
+++ components/TechStackPanel/TechStackPanel.tsx
@@ -1,13 +1,23 @@
 import { motion } from "framer-motion";
+import Image from "next/image";
+import techStack, { TechInfo } from "./TechStackData";
 
 const TechStackPanel = () => {
   return (
-    <div className="relative w-full h-full overflow-hidden bg-neutral">
-      <div className="w-3/4">
-        <ul>
-          <li></li>
-        </ul>
-      </div>
+    <div className="relative w-full h-full flex justify-center items-center overflow-hidden bg-neutral">
+      <ul className="w-3/5 h-2/3">
+        {techStack.map((t: TechInfo) => (
+          <li className="flex w-full h-[calc(100%/7)] mt-2 mb-2 border border-primary rounded-lg">
+            <span className="relative w-[5%] m-1 ml-2">
+              <Image src={t.icon} alt={t.name} fill />
+            </span>
+            <span className="text-center self-center text-lg font-bold ">{t.name}</span>
+            <span className="flex-grow text-end self-center mr-3 text-lg">
+              {t.techs.join(" , ")}
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
