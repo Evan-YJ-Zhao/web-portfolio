254d8987a660001ac94d3570a17a799106fb9781
Yu-Jun-Zhao <yujun.zhao4@gmail.com>
2024-11-07 16:44:18 -0800

    Completed section wrapper

--- components/Wrapper/SectionWrapper.tsx
+++ components/Wrapper/SectionWrapper.tsx
@@ -1,18 +1,26 @@
-import { OptionalClassName } from '@/utils/commonTypes'
-
+import {motion} from "framer-motion";
+import { OptionalClassName } from "@/utils/commonTypes";
 
 type Props = OptionalClassName & {
   children: React.ReactNode;
   title: string;
-} 
+};
 
-const SectionWrapper = ({children, className, title} : Props) => {
+const SectionWrapper = ({ children, className, title }: Props) => {
   return (
     <section className={`${className}`}>
-      <h2 className="absolute">{title}</h2>
+      <motion.div
+        className="absolute left-[5%] top-[10%] max-w-[10%] max-h-[10%] 
+          border border-primary rounded-lg box-content shadow-md overflow-hidden z-[1]"
+        initial={{opacity: 0, x: -50}}
+        animate={{opacity: 1, x: 0}}
+      >
+        <h2 className="m-1 font-bold text-lg text-wrap">{title}</h2>
+      </motion.div>
+
       {children}
     </section>
-  )
-}
+  );
+};
 
-export default SectionWrapper
+export default SectionWrapper;
