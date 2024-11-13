38422af4a96f9239cc7b8728ad3b1d41b2195eb0
Yu-Jun-Zhao <yujun.zhao4@gmail.com>
2024-11-12 18:12:45 -0800

    Created a memoized component for the switch button

--- app/(portfolio)/about-me/page.tsx
+++ app/(portfolio)/about-me/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import { useCallback, useState } from "react";
 import ImageSlider from "@/components/ImageSlider/ImageSlider";
 import SectionWrapper from "@/components/Wrapper/SectionWrapper";
+import SwitchMotionButton from "@/components/MotionButton/SwitchMotionButton";
 import TechStackPanel from "@/components/TechStackPanel/TechStackPanel";
 import useWindowAttr from "@/hooks/useWindowAttr";
 
@@ -12,22 +13,26 @@ const page = () => {
   const { isInClient, windowSize } = useWindowAttr();
   const { width } = windowSize;
 
-  if(!isInClient){
+  const imageSwitchButtonOnClickHandler = useCallback(() => {
+    setShowImageSlider((prev) => !prev);
+  }, []);
+
+  if (!isInClient) {
     return null;
-  }else if(width < 1024){
+  } else if (width < 1024) {
     // mobile view
     return (
       <>
-        <SectionWrapper className="relative w-full h-dvh bg-neutral" title="SKILLS" titlePosition="center">
+        <SectionWrapper
+          className="relative w-full h-dvh bg-neutral"
+          title="SKILLS"
+          titlePosition="center"
+        >
           <div></div>
         </SectionWrapper>
       </>
     );
   }
-
-  const imageSwitchButtonOnClickHandler = () => {
-    setShowImageSlider((prev) => !prev);
-  };
 
   return (
     <>
@@ -44,29 +49,10 @@ const page = () => {
           )}
         </AnimatePresence>
 
-        <div className="absolute bottom-0 right-[2%]">
-          <motion.button
-            className="w-10 h-10 m-4"
-            onClick={imageSwitchButtonOnClickHandler}
-            whileHover={{
-              scale: 1.2,
-            }}
-            whileTap={{ scale: 0.9 }}
-            transition={{ type: "spring", stiffness: 200, damping: 10 }}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-              <motion.path
-                d="M320-280h320v-400H320v400Zm80-80v-240h160v240H400Zm40-120h80v-80h-80v80ZM202.87-111.87q-37.78 0-64.39-26.61t-26.61-64.39v-554.26q0-37.78 26.61-64.39t64.39-26.61h554.26q37.78 0 64.39 26.61t26.61 64.39v554.26q0 37.78-26.61 64.39t-64.39 26.61H202.87Z"
-                initial={{
-                  pathLength: 0,
-                  fill: "#5f6368",
-                }}
-                animate={{ pathLength: 1, fill: "#000000" }}
-                transition={{ duration: 0.4 }}
-              />
-            </svg>
-          </motion.button>
-        </div>
+        <SwitchMotionButton
+          className="absolute bottom-0 right-[2%]"
+          clickHandler={imageSwitchButtonOnClickHandler}
+        />
       </SectionWrapper>
     </>
   );
