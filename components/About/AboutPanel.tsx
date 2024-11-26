747b10fbb979162cd65c1717113d5fb20573d881
Yu-Jun-Zhao <yujun.zhao4@gmail.com>
2024-11-25 20:01:05 -0800

    Added a dark mask when hover

--- components/About/AboutPanel.tsx
+++ components/About/AboutPanel.tsx
@@ -4,13 +4,19 @@ import Image from "next/image";
 const AboutPanel = () => {
   return (
     <div className="relative w-full h-full flex justify-center flex-row-reverse my-12 gap-10">
-      <div className="relative aspect-square">
-        <Image
-          src={aboutMeImg}
-          alt="Profile Image"
-          className="border border-primary rounded-full"
-          fill
-        />
+      <div className="relative indicator aspect-square rounded-full hover:bg-black">
+        <a href="/contact-me">
+          <span className="indicator-item indicator-center badge badge-primary">
+            Click me to contact
+          </span>
+          <Image
+            src={aboutMeImg}
+            alt="Profile Image"
+            className="border border-primary rounded-full 
+              hover:shadow-lg hover:shadow-primary hover:border-2 hover:opacity-70"
+            fill
+          />
+        </a>
       </div>
       <div className="px-10 flex flex-col justify-center">
         <h1 className="text-4xl font-bold text-right">(Evan) Yu Jun Zhao</h1>
@@ -19,14 +25,21 @@ const AboutPanel = () => {
           I'm not a designer.
         </p>
         <div className="flex justify-end gap-5">
-          <a href="https://github.com/Yu-Jun-Zhao/" target="_blank" className="relative btn aspect-square">
+          <a
+            href="https://github.com/Yu-Jun-Zhao/"
+            target="_blank"
+            className="relative btn aspect-square"
+          >
             <Image src={githubNamedImg} alt="Link to my Github" fill />
           </a>
-          <a href="https://www.linkedin.com/in/yu-jun-zhao/" target="_blank" className="relative btn aspect-square">
+          <a
+            href="https://www.linkedin.com/in/yu-jun-zhao/"
+            target="_blank"
+            className="relative btn aspect-square"
+          >
             <Image src={linkedInColoredImg} alt="Link to my LinkedIn" fill />
           </a>
         </div>
-        
       </div>
     </div>
   );
