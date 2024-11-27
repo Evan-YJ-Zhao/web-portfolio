ed20d30180cbfd17a278878bcd66c681af7ef47e
Yu-Jun-Zhao <yujun.zhao4@gmail.com>
2024-11-26 19:19:32 -0800

    Added a profile bio. Added a little responsiveness to it.

--- components/About/AboutPanel.tsx
+++ components/About/AboutPanel.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const AboutPanel = () => {
   return (
     <div className="relative w-full flex flex-wrap justify-center flex-row-reverse mt-12 mb-16 gap-10">
-      <div className="group relative indicator w-48 h-48 aspect-square rounded-full hover:bg-black">
+      <div className="w-48 h-48 group relative self-center indicator aspect-square rounded-full hover:bg-black">
         <a href="/contact-me">
           <span className="indicator-item indicator-center badge badge-primary rounded-lg group-hover:hidden">
             Click me to contact
@@ -18,11 +18,21 @@ const AboutPanel = () => {
           />
         </a>
       </div>
-      <div className="px-10 flex flex-col justify-center">
-        <h1 className="text-4xl font-bold text-center tablet:text-right">(Evan) Yu Jun Zhao</h1>
+      <div
+        className="w-11/12 tablet:w-3/5 laptop:w-1/2 
+          tablet:px-2 laptop:px-10 
+          flex flex-col justify-center"
+      >
+        <h1 className="text-4xl font-bold text-center tablet:text-right">
+          (Evan) Yu Jun Zhao
+        </h1>
         <p className="py-6 text-lg text-center tablet:text-right ">
-          I'm a Full-Stack Software Engineer. <br />
-          I'm not a designer.
+          I'm a full-stack software engineer based in the San Francisco Bay Area
+          with a Bachelor's in Computer Science from San José State University.
+          When I'm not coding, you'll find me reading web novels, trying to
+          outlift my last gym session, or indulging in food that makes me
+          question my time at the gym. Well, at the end, life is all about
+          balance, right?
         </p>
         <div className="flex justify-center tablet:justify-end gap-5">
           <a
