dd24b83f4ec194faa284975eee10d174dbd0b832
Evan-YJ-Zhao <yujun.zhao4@gmail.com>
2024-12-08 21:58:00 -0800

    Created a mailcontactbutton to replace the corresponding button in the contact-me page

--- app/(portfolio)/contact-me/page.tsx
+++ app/(portfolio)/contact-me/page.tsx
@@ -4,7 +4,8 @@ import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import { useState } from "react";
 import ContactForm from "@/components/contact-me/ContactForm";
-import { linkedInColoredImg, mailImg } from "@/utils/images";
+import MailContactButton from "@/components/contact-me/MotionButtons/MailContactButton";
+import { linkedInColoredImg } from "@/utils/images";
 import { LINKS } from "@/utils/links";
 
 const page = () => {
@@ -54,47 +55,7 @@ const page = () => {
               />
             </motion.a>
 
-            <motion.button
-              data-theme="cmyk"
-              className="w-14 h-14 btn btn-circle btn-ghost hover:bg-info"
-              onClick={messageButtonClickHandler}
-              initial={{ opacity: 0, x: -500, border: "2px solid transparent" }}
-              animate={{
-                opacity: 1,
-                x: 0,
-                border: [
-                  "2px solid transparent",
-                  "2px solid #FAFAFA",
-                  "2px solid #333333",
-                  "2px solid #FAFAFA",
-                  "2px solid transparent",
-                ],
-                transition: {
-                  opacity: {
-                    delay: 2,
-                  },
-                  x: {
-                    duration: 0.1,
-                    delay: 2,
-                  },
-                  border: {
-                    duration: 1.5,
-                    delay: 2.1,
-                  },
-                },
-              }}
-              exit={{ opacity: 0, x: 500 }}
-              whileTap={{
-                scale: 0.8,
-              }}
-            >
-              <Image
-                src={mailImg}
-                alt="Contact Form Image"
-                width={35}
-                height={35}
-              />
-            </motion.button>
+            <MailContactButton className="w-14 h-14" clickHandler={messageButtonClickHandler}/>
           </div>
         ) : (
           <ContactForm
---
+++ components/contact-me/MotionButtons/MailContactButton.tsx
@@ -0,0 +1,55 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { MouseEventHandler } from "react";
+import { OptionalClassName } from "@/utils/types";
+import { mailImg } from "@/utils/images";
+
+type Props = OptionalClassName & {
+  clickHandler: MouseEventHandler<HTMLButtonElement>;
+  iconWidth?: number;
+  iconHeight?: number;
+};
+
+// Use className to set the size of the button
+const MailContactButton = ({ className, clickHandler, iconWidth = 35, iconHeight = 35 }: Props) => {
+  return (
+    <motion.button
+      data-theme="cmyk"
+      className={`${className} btn btn-circle btn-ghost hover:bg-info`}
+      onClick={clickHandler}
+      initial={{ opacity: 0, x: -500, border: "2px solid transparent" }}
+      animate={{
+        opacity: 1,
+        x: 0,
+        border: [
+          "2px solid transparent",
+          "2px solid #FAFAFA",
+          "2px solid #333333",
+          "2px solid #FAFAFA",
+          "2px solid transparent",
+        ],
+        transition: {
+          opacity: {
+            delay: 2,
+          },
+          x: {
+            duration: 0.1,
+            delay: 2,
+          },
+          border: {
+            duration: 1.5,
+            delay: 2.1,
+          },
+        },
+      }}
+      exit={{ opacity: 0, x: 500 }}
+      whileTap={{
+        scale: 0.8,
+      }}
+    >
+      <Image src={mailImg} alt="Contact Form Image" width={iconWidth} height={iconHeight} />
+    </motion.button>
+  );
+};
+
+export default MailContactButton;
