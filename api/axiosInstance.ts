f17789e96f7ccfafb226a75be06f9815d3539931
Evan-YJ-Zhao <yujun.zhao4@gmail.com>
2024-12-10 17:20:26 -0800

    Added env variables and files

--- api/axiosInstance.ts
+++ api/axiosInstance.ts
@@ -3,7 +3,7 @@ import { ApiError } from "./errors";
 import { ErrorType } from "./types";
 
 const axiosInstance = axios.create({
-  baseURL: "/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
