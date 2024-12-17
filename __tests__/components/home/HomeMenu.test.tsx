a90adc491ad9accda6f217ea10792ae936a57b60
Evan-YJ-Zhao <yujun.zhao4@gmail.com>
2024-12-16 18:38:20 -0800

    HomeMenu unit test done

--- __tests__/components/home/HomeMenu.test.tsx
+++ __tests__/components/home/HomeMenu.test.tsx
@@ -1,7 +1,42 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import HomeMenu from "@/components/home/HomeMenu";
+import navLinks from "@/components/layout/Nav/NavLinks";
 
 describe("HomeMenu component", () => {
-  
+  it("should render a nav element", () => {
+    render(<HomeMenu />);
+
+    const nav = screen.getByRole("navigation");
+    expect(nav).toBeInTheDocument();
+  });
+
+  it("should render a list of links", () => {
+    render(<HomeMenu />);
+
+    const list = screen.getByRole("list");
+    expect(list).toBeInTheDocument();
+
+    const listItems = screen.getAllByRole("listitem");
+    expect(listItems).toHaveLength(navLinks.length);
+
+    navLinks.forEach((item, index) => {
+      const { name, isDisabled } = item;
+
+      const link = screen.getByRole("link", { name });
+      const listItem = link.closest("li");
+
+      // each anchor link should have li as the parent.
+      expect(listItem).toBeInTheDocument();
+
+      if (isDisabled) {
+        expect(listItem).toHaveClass("disabled");
+        expect(link).toHaveClass("btn-main-menu-gray-effect hover:cursor-not-allowed");
+      } else{
+        expect(listItem).not.toHaveClass("disabled");
+        expect(link).not.toHaveClass("hover:cursor-not-allowed");
+        expect(link).toHaveClass("btn-main-menu-gradient-effect");
+      }
+    });
+  });
 });
