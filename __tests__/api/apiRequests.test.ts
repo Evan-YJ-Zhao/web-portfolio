089fdfc6468949fbcb7bc9e1971185b474af9423
Evan-YJ-Zhao <yujun.zhao4@gmail.com>
2024-12-20 10:27:32 -0800

    apiRequests postRequest test done

--- __tests__/api/apiRequests.test.ts
+++ __tests__/api/apiRequests.test.ts
@@ -51,5 +51,41 @@ describe("apiRequests", () => {
     });
   });
 
-  describe("postRequest", () => {});
+  describe("postRequest", () => {
+    it("should return success true and data on resolved response", async () => {
+      const mockResponse = {
+        data: { id: 1, message: "This is a test" },
+        status: 201,
+        statusText: "OK",
+      };
+
+      mockedAxiosInstance.post.mockResolvedValueOnce(mockResponse);
+      const result: ApiResponse<TestResponseData> =
+        await postRequest<TestResponseData>("url", { test: "test" });
+
+      expect(result.success).toBe(true);
+      if (result.success) {
+        expect(result.data).toEqual(mockResponse.data);
+      }
+    });
+
+    it("should return success false and error on rejected ApiError, UnexpectedStatusCodeError", async () => {
+      const errorList = [
+        new ApiError("Test error", ErrorType.BAD_REQUEST),
+        new UnexpectedStatusCodeError(505),
+      ];
+
+      for (const error of errorList) {
+        mockedAxiosInstance.post.mockRejectedValueOnce(error);
+        const result: ApiResponse<TestResponseData> =
+          await postRequest<TestResponseData>("url", { test: "test" });
+
+        expect(result.success).toBe(false);
+        if (!result.success) {
+          expect(result.error.type).toBe(error.errorType);
+          expect(result.error.message).toBe(error.message);
+        }
+      }
+    });
+  });
 });
