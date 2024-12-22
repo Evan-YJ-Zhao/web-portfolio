0cf282ebbceb8476236e0e7123bf8f9a638ec6eb
Evan-YJ-Zhao <yujun.zhao4@gmail.com>
2024-12-21 16:46:00 -0800

    Removed unneeded images

--- utils/images.ts
+++ utils/images.ts
@@ -17,23 +17,23 @@ import queryStatsImg from "@/public/images/common/query_stats.svg";
 // Icons 
 import awsImg from "@/public/images/techstack/aws_colored.svg";
 import cloudflareImg from "@/public/images/techstack/cloudflare.svg";
-import css3Img from "@/public/images/techstack/css3_colored.svg";
+// import css3Img from "@/public/images/techstack/css3_colored.svg";
 import dockerImg from "@/public/images/techstack/docker_colored.svg";
 import framerMotionImg from "@/public/images/techstack/framer_motion.svg";
 import githubImg from "@/public/images/techstack/github.svg"
-import googlecloudImg from "@/public/images/techstack/googlecloud_colored.svg";
+// import googlecloudImg from "@/public/images/techstack/googlecloud_colored.svg";
 import html5Img from "@/public/images/techstack/html5_colored.svg";
-import javaImg from "@/public/images/techstack/java_colored.svg";
-import javaScriptImg from "@/public/images/techstack/javascript_colored.svg";
+// import javaImg from "@/public/images/techstack/java_colored.svg";
+// import javaScriptImg from "@/public/images/techstack/javascript_colored.svg";
 import mongoDBImg from "@/public/images/techstack/mongodb.svg";
-import mySQLImg from "@/public/images/techstack/mysql_colored.svg";
+// import mySQLImg from "@/public/images/techstack/mysql_colored.svg";
 import nextJsImg from "@/public/images/techstack/nextjs.svg";
 import nodeJsImg from "@/public/images/techstack/nodejs_colored.svg";
 import postmanImg from "@/public/images/techstack/postman.svg";
-import rabbitMQImg from "@/public/images/techstack/rabbitmq_colored.svg";
-import reactImg from "@/public/images/techstack/react_colored.svg";
-import redisImg from "@/public/images/techstack/redis_colored.svg";
-import splunkImg from "@/public/images/techstack/splunk.svg";
+// import rabbitMQImg from "@/public/images/techstack/rabbitmq_colored.svg";
+// import reactImg from "@/public/images/techstack/react_colored.svg";
+// import redisImg from "@/public/images/techstack/redis_colored.svg";
+// import splunkImg from "@/public/images/techstack/splunk.svg";
 import tailwindCSSImg from "@/public/images/techstack/tailwindcss_colored.svg";
 import typeScriptImg from "@/public/images/techstack/typescript_colored.svg";
 
@@ -54,23 +54,23 @@ export {
 
   awsImg,
   cloudflareImg,
-  css3Img,
+  // css3Img,
   dockerImg,
   framerMotionImg,
   githubImg,
-  googlecloudImg,
+  // googlecloudImg,
   html5Img,
-  javaImg,
-  javaScriptImg,
+  // javaImg,
+  // javaScriptImg,
   mongoDBImg,
-  mySQLImg,
+  // mySQLImg,
   nextJsImg,
   nodeJsImg,
   postmanImg,
-  rabbitMQImg,
-  reactImg,
-  redisImg,
-  splunkImg,
+  // rabbitMQImg,
+  // reactImg,
+  // redisImg,
+  // splunkImg,
   tailwindCSSImg,
   typeScriptImg,
 };
